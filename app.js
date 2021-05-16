9f5cf0fcad84b98420f9debc66566be33e0f9d18
Mechirv <mechivivanco@hotmail.com>
2021-05-16 14:38:23 -0300

    mechi

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname,'./public')));
-app.listen(3030, () => console.log("sever start in http://localhost:3030"));
+app.listen(3030, () => console.log("sever start in http:\\localhost:3030"));
 
 app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"/views","index.html")));
-app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"/views","login.html")));
+app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"/views/login.html")));
 app.get("/", (req,res)=> res.sendFile(path.join(__dirname,"/views","register.html")));
 app.get("/", (req,res)=> res.sendFile(path.join(__dirname,"/views","productCart.html")));
 app.get("/", (req,res)=> res.sendFile(path.join(__dirname,"/views","productDetails.html")));
