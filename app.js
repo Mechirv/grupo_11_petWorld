ba868f6c42e88316b81c8512123e0ca4a08a7e2e
Mechirv <mechivivanco@hotmail.com>
2021-05-17 13:49:14 -0300

    commit mechi

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.resolve(__dirname,'./public')));
+app.use(express.static(path.resolve(__dirname,'./public'))); //DEJO ACCESIBLE LA CARPETA PUBLIC
 app.listen(3030, () => console.log("sever start in http:\\localhost:3030"));
 
 app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"/views","index.html")));
-app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"/views","login.html")));
-app.get("/", (req,res)=> res.sendFile(path.join(__dirname,"/views","register.html")));
-app.get("/", (req,res)=> res.sendFile(path.join(__dirname,"/views","productCart.html")));
-app.get("/", (req,res)=> res.sendFile(path.join(__dirname,"/views","productDetails.html")));
+app.get("/login", (req,res)=>res.sendFile(path.join(__dirname,"/views","login.html")));
+app.get("/register", (req,res)=> res.sendFile(path.join(__dirname,"/views","register.html")));
+app.get("/productCart", (req,res)=> res.sendFile(path.join(__dirname,"/views","productCart.html")));
+app.get("/productDetails", (req,res)=> res.sendFile(path.join(__dirname,"/views","productDetails.html")));
---
+++ retro.md
@@ -0,0 +1,13 @@
+Cosas que hicimos bien en el primer sprint
+-coordinación de las tareas a realizar
+buena comunicación
+
+
+Qué hicimos mal
+-Pocas reuniones
+
+Que deberíamos comenzar a hacer-
+-Reuniones periódicas
+
+
+Que debemos dejar de hacer
