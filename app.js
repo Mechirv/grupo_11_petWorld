b0ddbb0a97071a493f680ae2564dbc330fae7e73
Fakundus01 <facumoreno2001@gmail.com>
2021-05-25 16:49:28 -0300

    Agrego el productCart y la base del productDetails

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname,'./public'))); //DEJO ACCESIBLE LA CARPETA PUBLIC
-app.listen(3030, () => console.log("sever start in http:\\localhost:3030"));
+app.listen(3030, () => console.log("sever start in http://localhost:3030"));
 
 app.get("/", (req,res)=>res.sendFile(path.join(__dirname,"/views","index.html")));
 app.get("/login", (req,res)=>res.sendFile(path.join(__dirname,"/views","login.html")));
