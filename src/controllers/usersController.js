505e93636331339b2393af2396e6a8b2e6708425
Mechirv <mechivivanco@hotmail.com>
2021-06-19 15:29:05 -0300

    mechi

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const usersControlador = {
     login: (req,res)=>res.render('users/login'),
-    register: (req,res) => res.render('register'),
+    register: (req,res) => res.render('users/register'),
  
 
 }
